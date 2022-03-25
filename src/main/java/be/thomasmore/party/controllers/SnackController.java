2d5a5e14c93d575880775e6de4f2264cba50df3f
Maarten <maarten.gillis1@gmail.com>
2022-03-25 09:39:33 +0100

    oefening 3, ondertekst side dish staat nog niet goed

--- src/main/java/be/thomasmore/party/controllers/SnackController.java
+++ src/main/java/be/thomasmore/party/controllers/SnackController.java
@@ -1,7 +1,37 @@
 package be.thomasmore.party.controllers;
 
+import be.thomasmore.party.model.Snack;
+import be.thomasmore.party.repositories.SnackRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
 
 @Controller
 public class SnackController {
+    @Autowired
+    private SnackRepository snackRepository;
+
+    @GetMapping({"/snacklist", "/snacklist/{extrawoord}"})
+    public String snacklist(Model model, @PathVariable(required = false) String extrawoord){
+        Iterable<Snack> snacksFromDb = snackRepository.findAll();
+        model.addAttribute("snacks", snacksFromDb);
+        long nrOfSnacks = snackRepository.count();
+        model.addAttribute("nrOfSnacks", nrOfSnacks);
+        return "snacklist";
+    }
+
+    @GetMapping("/snackdetails/{id}")
+    public String snackdetails(Model model, @PathVariable Integer id){
+        if(id == null) return "snackdetails";
+
+        Optional<Snack> snackFromDb = snackRepository.findById(id);
+        if(snackFromDb.isPresent()) {
+            model.addAttribute("snack", snackFromDb);
+        }
+        return "snackdetails";
+    }
 }
--- src/main/java/be/thomasmore/party/model/Snack.java
+++ src/main/java/be/thomasmore/party/model/Snack.java
@@ -14,4 +14,52 @@ public class Snack {
     //Double met grote letter kan je "null" maken in de database
     //terwijl een double met kleine letter dat niet kan dus je moet hier wel een Double met grote letter gebruiken
     private Double priceSideDish;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isVegan() {
+        return vegan;
+    }
+
+    public void setVegan(boolean vegan) {
+        this.vegan = vegan;
+    }
+
+    public boolean isSideDishPossible() {
+        return sideDishPossible;
+    }
+
+    public void setSideDishPossible(boolean sideDishPossible) {
+        this.sideDishPossible = sideDishPossible;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Double getPriceSideDish() {
+        return priceSideDish;
+    }
+
+    public void setPriceSideDish(Double priceSideDish) {
+        this.priceSideDish = priceSideDish;
+    }
 }
