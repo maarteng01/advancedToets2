05e6d1141596e46b248ecad138684c3af65d91b9
Maarten <maarten.gillis1@gmail.com>
2022-03-25 10:19:03 +0100

    oefening 4

--- src/main/java/be/thomasmore/party/controllers/SnackController.java
+++ src/main/java/be/thomasmore/party/controllers/SnackController.java
@@ -1,5 +1,6 @@
 package be.thomasmore.party.controllers;
 
+import be.thomasmore.party.model.Party;
 import be.thomasmore.party.model.Snack;
 import be.thomasmore.party.repositories.SnackRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,14 +25,38 @@ public class SnackController {
         return "snacklist";
     }
 
-    @GetMapping("/snackdetails/{id}")
-    public String snackdetails(Model model, @PathVariable Integer id){
+    @GetMapping({"/snackdetails", "/snackdetails/{id}"})
+    public String snackdetails(Model model, @PathVariable(required = false) Integer id){
         if(id == null) return "snackdetails";
-
+        long nrOfSnacks = snackRepository.count();
+        model.addAttribute("nrOfSnacks", nrOfSnacks);
+        if(id > nrOfSnacks) return "snackIdError";
         Optional<Snack> snackFromDb = snackRepository.findById(id);
         if(snackFromDb.isPresent()) {
-            model.addAttribute("snack", snackFromDb);
+            model.addAttribute("snack", snackFromDb.get());
         }
+        return "snackdetails";
+    }
+
+    @GetMapping({"/snackdetails/{id}/prev"})
+    public String snackDetailsPrev(Model model, @PathVariable int id) {
+        Optional<Snack> prevSnackFromDb = snackRepository.findFirstByIdLessThanOrderByIdDesc(id);
+        if (prevSnackFromDb.isPresent())
+            return String.format("redirect:/snackdetails/%d", prevSnackFromDb.get().getId());
+        Optional<Snack> lastSnackFromDb = snackRepository.findFirstByOrderByIdDesc();
+        if (lastSnackFromDb.isPresent())
+            return String.format("redirect:/snackdetails/%d", lastSnackFromDb.get().getId());
+        return "snackdetails";
+    }
+
+    @GetMapping({"/snackdetails/{id}/next"})
+    public String partydetailsNext(Model model, @PathVariable int id) {
+        Optional<Snack> nextSnackFromDb = snackRepository.findFirstByIdGreaterThanOrderByIdAsc(id);
+        if (nextSnackFromDb.isPresent())
+            return String.format("redirect:/snackdetails/%d", nextSnackFromDb.get().getId());
+        Optional<Snack> firstSnackFromDb = snackRepository.findFirstByOrderByIdAsc();
+        if (firstSnackFromDb.isPresent())
+            return String.format("redirect:/snackdetails/%d", firstSnackFromDb.get().getId());
         return "snackdetails";
     }
 }
--- src/main/java/be/thomasmore/party/repositories/SnackRepository.java
+++ src/main/java/be/thomasmore/party/repositories/SnackRepository.java
@@ -1,7 +1,17 @@
 package be.thomasmore.party.repositories;
 
+import be.thomasmore.party.model.Party;
 import be.thomasmore.party.model.Snack;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface SnackRepository extends CrudRepository<Snack, Integer> {
+    Optional<Snack> findFirstByIdLessThanOrderByIdDesc(Integer id);
+
+    Optional<Snack> findFirstByOrderByIdDesc();
+
+    Optional<Snack> findFirstByIdGreaterThanOrderByIdAsc(Integer id);
+
+    Optional<Snack> findFirstByOrderByIdAsc();
 }
